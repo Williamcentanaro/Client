80ceeec9a969e653df79ff087a160944638d3fc5
William <williamcenta01@gmail.com>
2021-03-02 14:08:44 +0100

    commit

--- ProgramManager/Assets/Script/JsonReader.cs
+++ ProgramManager/Assets/Script/JsonReader.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
 
 
 public class JsonReader : MonoBehaviour
 {
+  
     public TextAsset textJSON;
     [System.Serializable]
     public class Player
@@ -25,6 +32,9 @@ public class JsonReader : MonoBehaviour
     public PlayerList myPlayerList = new PlayerList();
     public void Start()
     {
+        //Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //IPEndPoint connecting = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 52844);
+        //listen.Connect(connecting);
         string json = JsonUtility.ToJson(myPlayerList);
         //myPlayerList = JsonUtility.FromJson<PlayerList>(json);
         Debug.Log(json);
