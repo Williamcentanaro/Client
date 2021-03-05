deb9573e6fab8fc78fc0fe2806c96b5e394afdb4
Anthony Avila <g.avilaguarnizo@reply.it>
2021-03-05 21:29:34 +0100

    test

---
+++ ProgramManager/Assets/Script/OptionsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OptionsPlayer : MonoBehaviour
+{
+    public int x { get; set; }
+    public int w { get; set; }
+    public int h { get; set; }
+    public int y { get; set; }
+    public string fullScreen { get; set; }
+
+    public static OptionsPlayer CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<OptionsPlayer>(jsonString);
+    }
+
+}
